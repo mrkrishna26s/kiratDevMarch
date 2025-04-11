f5daff110d8d6624bffe957574559cc7fe4bac06
mekrishna26s <murari2632000@gmail.com>
2025-04-11 06:07:24 +0530

    added code of less ugly reconciler

---
+++ week4/week4.1/lessUglyTodo.js
@@ -0,0 +1,60 @@
+function createDomElement(data){
+    var parentElement = document.getElementById("mainArea1");
+    var currentChildren = Array.from(parentElement.children);
+    console.log(currentChildren);
+    let added = 0, deleted =0, updated =0;
+    data.forEach(function(item){
+        var existingChild = currentChildren.find(function (child){
+            return child.dataset.id === String(item.id);
+        });
+        if(existingChild){
+            updated++;
+            existingChild.children[0].innerHTML = item.title;
+            existingChild.children[1].innerHTML= item.description;
+            currentChildren= currentChildren.filter(function(child){
+                return child != existingChild;
+            });
+        }else{
+            added++;
+            var childElement = document.createElement("div");
+            childElement.dataset.id = item.id;
+
+            var grandChildElement1 = document.createElement("span");
+            grandChildElement1.innerHTML = item.title;
+
+            var grandChildElement2 = document.createElement("span")
+            grandChildElement2.innerHTML = item.description;
+
+            var grandChildElement3 = document.createElement("button");
+            grandChildElement3.innerHTML = "delete";
+            grandChildElement3.setAttribute("onclick", "deleTodo(" + item.id +")");
+
+            parentElement.appendChild(childElement);
+
+
+            childElement.appendChild(grandChildElement1);
+            childElement.appendChild(grandChildElement2);
+            childElement.appendChild(grandChildElement3);
+        }
+    });
+    currentChildren.forEach(function(child){
+        deleted++;
+        parentElement.removeChild(child);
+    });
+    console.log(added);
+    console.log(deleted);
+    console.log(updated)
+}
+
+
+window.setInterval(()=> {
+    const  todos = [];
+    for(let i =0; i< Math.floor(Math.random()*100); i++){
+        todos.push({
+            title: "go to gym",
+            description: "go to gym from 5",
+            id: i+1
+        })
+    }
+    createDomElement(todos)
+},1000);
